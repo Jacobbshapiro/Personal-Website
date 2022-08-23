0c0b106a115ac955cc3d9809669b51123f59d2d7
Jacobbshapiro <jacobbshapiro7@gmail.com>
2022-08-23 09:16:29 -0700

    start

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,18 +1,8 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
-import Home from "../HomePage/Home"
-import NavBar from "../../components/NavBar/NavBar"
 
 function App() {
   return (
-    <main className="App">
-      <>
-      <NavBar />
-        <Routes>
-          <Route path="/Home" element={<Home />} />
-        </Routes>
-      </>
-    </main>
+    <h1>Hello</h1>
   )
 }
 
