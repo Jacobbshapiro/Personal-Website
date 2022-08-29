1082281f18a0fb7a4256268d7bbc4499ef414e2c
Jacobbshapiro <jacobbshapiro7@gmail.com>
2022-08-29 11:29:45 -0700

    basis of website down

--- src/pages/PortfolioPage/Portfolio.jsx
+++ src/pages/PortfolioPage/Portfolio.jsx
@@ -4,15 +4,21 @@ export default function Portfolio() {
             <div className="port-div">
                 <h1>Work</h1>
                 <div className="work-container">
-                    <div className="pokewiki">
-                        <h2>PokeWiki</h2>
-                    </div>
-                    <div className="yakesale">
-                        <h2>Yakesale</h2>
-                    </div>
-                    <div className="browgames">
-                        <h2>Browgames</h2>
-                    </div>
+                    <a href="https://pokeyake.herokuapp.com/" target="_blank">
+                        <div className="pokewiki">
+                            <h2>PokeWiki</h2>
+                        </div>
+                    </a>
+                    <a href="https://yakesale.herokuapp.com/items" target="_blank">
+                        <div className="yakesale">
+                            <h2>Yakesale</h2>
+                        </div>
+                    </a>
+                    <a href="https://browgames.herokuapp.com/" target="_blank">
+                        <div className="browgames">
+                            <h2>Browgames</h2>
+                        </div>
+                    </a>
                 </div>
             </div>
         </>
