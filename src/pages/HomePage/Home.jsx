5a345dea8d3ff7c51ec8c3093ef1d055583f45b7
Jacobbshapiro <jacobbshapiro7@gmail.com>
2022-09-28 11:45:13 -0700

    removed a photo

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -47,7 +47,6 @@ export default function Home() {
                 <div className="frame-img">
                     <img id="lang-img" src="https://cdn.worldvectorlogo.com/logos/django.svg" alt="img." />
                     <img id="lang-img" src="https://cdn.icon-icons.com/icons2/2699/PNG/512/expressjs_logo_icon_169185.png" alt="img." />
-                    <img id="lang-img" src="https://cdn.icon-icons.com/icons2/2415/PNG/512/jquery_plain_wordmark_logo_icon_146445.png" alt="img." />
                     <img id="lang-img" src="https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png" alt="img." />
                     <img id="lang-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="img." />
                 </div>
